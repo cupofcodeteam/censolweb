3443c98c75101d861a2075cd1d576742c55d6cef
Alex <alexis.jalisoft@gmail.com>
2017-04-30 10:31:46 -0400

    responsive js, refactor del script principal de l apagina

--- public/js/main.js
+++ public/js/main.js
@@ -1,99 +1,81 @@
 
-jQuery(function ($) {
+$(document).ready(function() {
+  if (isBreakPoint('xs')){
+    $('.appear-right').addClass('appear-left');
+    $('.appear-right').toggleClass('appear-right');
 
-    'use strict';
-
-    // ----------------------------------------------
-    // Table index
-    // ----------------------------------------------
-
-    /*-----------------------------------------------
-    # Dropdown Menu Animation
-    # Navigation Fixed
-    # Search
-	# Parallax Scrolling
-    # Slider Height
-    # Active mixitup
-    # smoothScroll
-    # Pretty Photo
-    # Single Portfolio
-    # Close Single Portfolio
-    # Timer
-    # Google Map Customization
-    -------------------------------------------------*/
-
-	 // ----------------------------------------------
-    // # Dropdown Menu Animation
-    // ----------------------------------------------
-
-	// ----------------------------------------------
-    // # Navigation fixed
-    // ----------------------------------------------
-
-
-	// ----------------------------------------------
-    // # Parallax Scrolling
-    // ----------------------------------------------
-
-    (function () {
-
-        function parallaxInit() {
-           $("#promotion").parallax("50%", 0.3);
-			$("#twitter").parallax("50%", 0.3);
-			$("#promotion-two").parallax("50%", 0.3);
-			$("#about-video").parallax("50%", 0.3);
-			$("#onepage #fun-fact").parallax("50%", 0.3);
-			$("#onepage #shop").parallax("50%", 0.3);
-			$("#landing #video-promotion").parallax("50%", 0.3);
-			$("#landing #feature-in").parallax("50%", 0.3);
-        }
-        parallaxInit();
-
-    }());
-
-	// ----------------------------------------------
-    // # Clients Carousel Timing
-    // ----------------------------------------------
-	(function () {
-
-		$('#client-carousel').carousel({
-		  interval: 4000
-		})
-    }());
-
-    $("home-top").ready(function(){
-      $("#home-top").css('height',$(window).height()*0.90);
-      $(window).resize(function() {
-          $("#home-top").css('height',$(window).height()*0.90);
-      });
-    });
-
-    $("intercambio-country").ready(function(){
-      $("#intercambio-country").css('height',$(window).height()*0.90);
-    });
-
-    $("intercambio-Vnzla").ready(function(){
-      $("#intercambio-Vnzla").css('height',$(window).height()*0.90);
-    });
-
-
-    var modal = $("#img-modal");
-
-    var img = $(".image");
-    var modalImg = $("#img01");
-    var captionText = $("#caption");
-
-    img.click(function(){
-      console.log("hola");
-      modal.css('display','block');
-      modalImg.attr('src',this.src);
-      captionText.html(this.alt);
-    });
-
-    var span = $(".close");
-
-    span.click(function() {
-      modal.css('display','none');
-    });
-
+    $('.appear-zoom').addClass('appear-left');
+    $('.appear-zoom').toggleClass('appear-zoom');
+  }else {
+    scrollingNav();
+  }
 });
+
+function parallaxInit() {
+  $("#promotion").parallax("50%", 0.3);
+  $("#twitter").parallax("50%", 0.3);
+  $("#promotion-two").parallax("50%", 0.3);
+  $("#about-video").parallax("50%", 0.3);
+  $("#onepage #fun-fact").parallax("50%", 0.3);
+  $("#onepage #shop").parallax("50%", 0.3);
+  $("#landing #video-promotion").parallax("50%", 0.3);
+  $("#landing #feature-in").parallax("50%", 0.3);
+}
+parallaxInit();
+
+
+  $('#client-carousel').carousel({
+    interval: 4000
+  });
+
+$("home-top").ready(function(){
+  $("#home-top").css('height',$(window).height()*0.90);
+  $(window).resize(function() {
+    $("#home-top").css('height',$(window).height()*0.90);
+  });
+});
+
+$("intercambio-country").ready(function(){
+  $("#intercambio-country").css('height',$(window).height()*0.90);
+});
+
+$("intercambio-Vnzla").ready(function(){
+  $("#intercambio-Vnzla").css('height',$(window).height()*0.90);
+});
+
+
+var modal = $("#img-modal");
+
+var img = $(".image");
+var modalImg = $("#img01");
+var captionText = $("#caption");
+
+img.click(function(){
+  modal.css('display','block');
+  modalImg.attr('src',this.src);
+  captionText.html(this.alt);
+});
+
+var span = $(".close");
+
+span.click(function() {
+  modal.css('display','none');
+});
+
+function isBreakPoint(bp) {
+  var w = $(window).width();
+  switch (bp) {
+    case 'xs':
+    return (w <= 767);
+    break;
+    case 'sm':
+    return (w >=  768 && w <= 991);
+    break;
+    case 'md':
+    return (w >=  992 && w <= 1199);
+    break;
+    case 'lg':
+    return (w >= 1400);
+    break;
+  }
+}
